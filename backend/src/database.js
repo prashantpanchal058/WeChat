720a9beb2276fb71f682d7008ab68229ac402975
Prashant-panchal05 <pp2575676@gmail.com>
2026-01-11 16:00:19 +0530

    change url database

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoURI = "mongodb+srv://prashantpanchal058_db_user:pWKSsslkwtew2XVL@cluster0.vzkusa2.mongodb.net/?appName=Cluster0/weChat";
+const mongoURI = "mongodb+srv://prashantpanchal058_db_user:pWKSsslkwtew2XVL@cluster0.vzkusa2.mongodb.net/weChat?appName=Cluster0";
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI)
