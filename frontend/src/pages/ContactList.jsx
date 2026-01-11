f408a1ecff1d6ef87d41db61299a25de417b6ec1
Prashant-panchal05 <pp2575676@gmail.com>
2026-01-11 23:57:14 +0530

    add name

--- frontend/src/pages/ContactList.jsx
+++ frontend/src/pages/ContactList.jsx
@@ -110,6 +110,10 @@ const ContactList = () => {
                                     className={`w-12 h-12 ${chat.pic? "": "p-2"} rounded-full bg-blue-200 object-cover`}
                                 />
 
+                                <div className="flex-1">
+                                    <p className="font-medium">{chat.name}</p>
+                                </div>
+
                                 {chat.online && (
                                     <span className="h-2 w-2 bg-green-500 rounded-full" />
                                 )}
